881a8d981ab0df48f7cebf83e6876cb651f22a3e
Aldo Vargas <aldo@aldostools.org>
2017-01-03 16:06:52 -0400

    webMAN MOD 1.45.07
    - Added check for current status to enable_netemu_cobra()

--- include/ps2_classic.h
+++ include/ps2_classic.h
@@ -1,9 +1,20 @@
 #ifdef COBRA_ONLY
 #define SYSCALL8_OPCODE_ENABLE_PS2NETEMU	0x1ee9	/* Cobra 7.50 */
+#define PS2NETEMU_GET_STATUS				2
+
+static int get_cobra_ps2netemu_status(void)
+{
+	system_call_2(SC_COBRA_SYSCALL8, (uint64_t) SYSCALL8_OPCODE_ENABLE_PS2NETEMU, (uint64_t) PS2NETEMU_GET_STATUS);
+	return (int)p1;
+}
 
 static void enable_netemu_cobra(int param)
 {
-	system_call_2(SC_COBRA_SYSCALL8, (uint64_t) SYSCALL8_OPCODE_ENABLE_PS2NETEMU, (uint64_t)param);
+	int status = get_cobra_ps2netemu_status();
+
+	if(status < 0 || status == param) return;
+
+	system_call_2(SC_COBRA_SYSCALL8, (uint64_t) SYSCALL8_OPCODE_ENABLE_PS2NETEMU, (uint64_t) param);
 }
 #endif
 
